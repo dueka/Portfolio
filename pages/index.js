0b106ad872f177b5806af8a9f03cfa8902ab099b
dueka <ekanemd@live.com>
2020-04-01 00:26:02 +0100

    added the footer

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import Header from "../components/Header";
 import Main from "../components/Main";
+import Footer from "../components/Footer";
+
 import "../assets/styles.scss";
 export default function Index() {
   return (
     <div>
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
